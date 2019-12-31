67ec12df47e56055cee96c0cb22b9ac373c48223
Matthew Greer <mgreer906@gmail.com>
2019-12-30 23:48:22 -0500

    Add files via upload

---
+++ asn_checker.py
@@ -0,0 +1,100 @@
+#!/usr/bin/env python3
+
+import requests
+import socket
+import sys
+import argparse
+
+def options():
+	parser = argparse.ArgumentParser()
+	# Allow user to search via IP
+	parser.add_argument(
+		"--ip",
+		help="Check ASN number from IP.",
+		action="store",
+	)
+	# Allow user to search via URL
+	parser.add_argument(
+		"--url",
+		help="Check ASN number from URL.",
+		action="store",
+	)
+	# Normal options just display ASN
+	# However, user can add --details option to print more information regarding the IP/URL
+	parser.add_argument(
+		"--details",
+		help="Display detailed information for ASN.",
+		action="store_true",
+	)
+
+	# if no arguments are given, print usage message
+	if len(sys.argv[1:]) == 0:
+		parser.print_help()
+		parser.exit()
+
+	args = parser.parse_args()
+
+	return args
+
+def return_ip():
+	if options().url:
+		try:
+			if options().url.startswith("http://"):
+				url = options().url[7:]
+				ip = socket.gethostbyname(url)
+				return ip
+
+			elif options().url.startswith("https://"):
+				url = options().url[8:]
+				ip = socket.gethostbyname(url)
+				return ip
+
+			else:
+				url = options().url
+				ip = socket.gethostbyname(url)
+				return ip
+
+		except socket.gaierror:
+			print("ERROR: Host not recognized!")
+			sys.exit(1)
+
+	elif options().ip:
+		ip = options().ip
+		return ip
+
+def asn():
+	try:
+		ip = return_ip()
+		api_url = f"https://api.iptoasn.com/v1/as/ip/{ip}"
+		response = requests.get(api_url)
+		data = response.json()
+
+		# data filters
+		announced = data["announced"]
+		country_code = data["as_country_code"]
+		description = data["as_description"]
+		ASN = data["as_number"]
+		first_IP = data["first_ip"]
+		IP = data["ip"]
+		last_IP = data["last_ip"]
+
+		if not options().details:
+			print(f"ASN\t{ASN}")
+
+		elif options().details:
+			print(f"Announced:   \t{announced}") #9
+			print(f"Country Code:\t{country_code}") #13
+			print(f"Description: \t{description}") #12
+			print(f"ASN:         \t{ASN}") #4
+			print(f"Starting IP: \t{first_IP}") #12
+			print(f"Ending IP:   \t{last_IP}") #10
+			print(f"IP Searched: \t{IP}") #12
+
+	except ValueError:
+		print("ERROR! ASN could not be parsed!")
+		print("Make sure you did not accidentially put URL for IP or vice versa.")
+		sys.exit(1)
+
+def main():
+	asn()
+main()
